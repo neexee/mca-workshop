9ddd56f255902502034cea28380918f5631b5b68
Artem Belyakov <belyakov.arteom2015@gmail.com>
2020-08-11 21:30:46 +0300

    domain_fixed

--- whois.py
+++ whois.py
@@ -1,20 +1,28 @@
 import json
 import requests
 import os
-from datetime import datetime, date
+from datetime import date
+import math
+
+sigma = 10
+a = 3
 
 HOST_SITE = "https://www.whoisxmlapi.com/whoisserver/WhoisService"
 API_key =  os.environ.get('WHOIS_KEY')
 
-def print_pretty_json(data):
-    print(json.dumps(data, indent=2, sort_keys=False, ensure_ascii=False))
 
 def get_domain_score(domain_name):
     return get(domain_name)
 def get_ip_score(ip):
     return get(ip)
 
-def get(domain_name): # by domain_name, ipv4 or ipv6
+def normal_distribution(x):
+    return (a * math.exp(-x / sigma) if a * math.exp(-x / sigma) < 1 else 1)
+
+def print_pretty_json(data):
+    print(json.dumps(data, indent=2, sort_keys=False, ensure_ascii=False))
+
+def get(domain_name):
         request = requests.get(f"{HOST_SITE}?apiKey={API_key}&domainName={domain_name}&outputFormat=JSON&ip=1")
         try:
             request.raise_for_status()
@@ -29,7 +37,7 @@ def get(domain_name): # by domain_name, ipv4 or ipv6
         date_of_creation = date(arr[0], arr[1], arr[2])
         existence_time = date.today() - date_of_creation
 
-        print(existence_time.days)
+        return (normal_distribution(existence_time.days))
 
 if __name__ == "__main__":
     get("google.com")
